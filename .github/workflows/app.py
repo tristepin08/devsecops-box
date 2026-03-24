6f9d7a55466c040030e307e07101ea74ee272048
tristepin08 <abdelsow91@gmail.com>
2026-03-24 17:34:43 +0100

    Correction de la faille SQLI AVEC UNE REQUETE PARAMETREE

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -3,17 +3,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# On crée une vraie page web accessible sur Internet
 @app.route('/recherche')
 def recherche_utilisateur():
-    # 1. LA SOURCE : On récupère ce que le visiteur tape dans l'URL
     nom_utilisateur = request.args.get('nom') 
     
     connexion = sqlite3.connect('base.db')
     curseur = connexion.cursor()
     
-    # 2. LE GOUFFRE : On injecte le texte du visiteur directement dans le SQL
-    requete = "SELECT * FROM users WHERE nom = '" + nom_utilisateur + "'"
-    curseur.execute(requete)
+    # 🛡️ LA CORRECTION EST ICI :
+    # 1. On utilise un "?" comme espace réservé
+    requete = "SELECT * FROM users WHERE nom = ?"
+    
+    # 2. On donne la variable à part, entre parenthèses
+    curseur.execute(requete, (nom_utilisateur,))
     
     return "Recherche terminée"
