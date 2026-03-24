8777135d381ae1b413439b65c15a27c1a31d9b28
tristepin08 <abdelsow91@gmail.com>
2026-03-24 17:18:42 +0100

    ajout d'une vraie interface web vulnerable

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,12 +1,19 @@
 import sqlite3
+from flask import Flask, request
 
-def trouver_utilisateur(nom_utilisateur):
-    connexion = sqlite3.connect('ma_base_de_donnees.db')
+app = Flask(__name__)
+
+# On crée une vraie page web accessible sur Internet
+@app.route('/recherche')
+def recherche_utilisateur():
+    # 1. LA SOURCE : On récupère ce que le visiteur tape dans l'URL
+    nom_utilisateur = request.args.get('nom') 
+    
+    connexion = sqlite3.connect('base.db')
     curseur = connexion.cursor()
     
-    # 🚨 ATTENTION : VOICI LA FAMEUSE INJECTION SQL !
-    # On colle directement le texte de l'utilisateur dans la requête
-    requete = "SELECT * FROM utilisateurs WHERE nom = '" + nom_utilisateur + "'"
-    
+    # 2. LE GOUFFRE : On injecte le texte du visiteur directement dans le SQL
+    requete = "SELECT * FROM users WHERE nom = '" + nom_utilisateur + "'"
     curseur.execute(requete)
-    return curseur.fetchall()
+    
+    return "Recherche terminée"
