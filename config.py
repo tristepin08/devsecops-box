b09a6f5f8a26d28ed867e10525f10f41c8524c36
tristepin08 <abdelsow91@gmail.com>
2026-03-25 15:26:33 +0100

    deuxieme fausse cle

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Fichier de configuration
-# ATTENTION : Ne jamais faire ça dans la vraie vie !
+# Cette fois-ci, ca a l'air d'etre une vraie cle !
 
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+AWS_ACCESS_KEY_ID = "AKIA5QYUX234Y7D2J4H9"
+AWS_SECRET_ACCESS_KEY = "vH9Pabcde12345FGHIJklmnopqrstuvwxyz0123"
