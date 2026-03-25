29cf0d4dd6b6c8f544becab28512f74c54af029c
tristepin08 <abdelsow91@gmail.com>
2026-03-25 15:18:18 +0100

    oublie de la clés aws test

---
+++ config.py
@@ -0,0 +1,5 @@
+# Fichier de configuration
+# ATTENTION : Ne jamais faire ça dans la vraie vie !
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
