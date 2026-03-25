92b9bbef58374551c460ede23de03d75c218c557
tristepin08 <abdelsow91@gmail.com>
2026-03-25 17:39:30 +0100

    deploiement final auto via webhook

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Projet DevSecOps Complet - Deploy Automatique OK ! ✅🥇</h1>"
+    return "<h1>🚀 MISSION ACCOMPLIE ! Déploiement 100% Automatique et Sécurisé. Félicitations Abdel ! 🛡️✅</h1>"
