76ac25f52afbde7abf4c36cdb715151f89734264
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-04-28 15:41:54 +0200

    New content

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 // nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage
-const app = express(); 
+const app = express();
 
-app.get('/download', (req, res) => {
-    // On renvoie juste un message fixe, sans aucune variable utilisateur
-    // Comme ça, il n'y a aucun risque de XSS ou de RCE.
-    res.json({
-        status: "success",
-        message: "Fichier prêt au téléchargement (Simulation sécurisée)"
-    });
+app.get("/download", (req, res) => {
+  // On renvoie juste un message fixe, sans aucune variable utilisateur
+  // Comme ça, il n'y a aucun risque de XSS ou de RCE.
+  res.json({
+    status: "success",
+    message: "Fichier prêt au téléchargement (Simulation sécurisée)",
+  });
 });
 
-app.get('/', (req, res) => {
-    res.send("<h1>Forteresse Validée</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>Forteresse Validéeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
