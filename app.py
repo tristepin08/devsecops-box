e6ba9c860b2f7bc582bdd8cb57ce24a845c42a04
tristepin08 <abdelsow91@gmail.com>
2026-04-28 10:21:29 +0200

    test auto 3

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # 🚨 Faille critique : on laisse l'utilisateur taper une commande système !
-    cmd = request.args.get('cmd')
-    os.system(cmd) 
-    return "Test du pipeline en cours..."
+    return "<h1>Bienvenue sur mon site sécurisé !</h1><p>Le pipeline DevSecOps fonctionne à 100%.</p>"
+
+@app.route('/health')
+def health_check():
+    # Une route "health" est un standard en entreprise pour vérifier que le serveur va bien
+    return {"status": "ok", "message": "Serveur opérationnel et sécurisé"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # On écoute le port imposé par Render, sinon on utilise le 5000 sur ton PC
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Règle d'or de sécurité : debug=False en production !
+    app.run(host='0.0.0.0', port=port, debug=False)
